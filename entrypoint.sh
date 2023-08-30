7f15f4992cc37c13c3e17ba5ab86c348a9f71ce7
mobmob123 <129247799+mobmob123@users.noreply.github.com>
2023-08-30 12:15:40 +0800

    Add files via upload
    touch

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,6 @@
+touch ./cert/private.key
+touch ./cert/certificate.crt 
+
 openssl req -x509 -newkey rsa:4096 -keyout ./cert/private.key -out ./cert/certificate.crt -days 365 -nodes -subj "/C=US/ST=California/L=San Francisco/O=YourOrganization/OU=YourDepartment/CN=yourdomain.com/emailAddress=your@email.com" 
 
 echo "const turnConfig = {iceServers: [{ urls: [\"stun:$turn_server_ip\"]},{   urls: \"turn:$turn_server_ip\",username: \"username\", credential: \"password\"}]};" > ./public/js/config.js
